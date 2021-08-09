2e91de095b3f54f5531fad6daee8d2aae0eba86f
BisiDavi <oludavidconnect@gmail.com>
2021-08-09 07:08:50 +0100

    page styled

--- packages/own-theme/src/home.js
+++ packages/own-theme/src/home.js
@@ -1,10 +1,36 @@
+import { Global, css, Head, connect } from "frontity";
+
 import Layout from "./layout/layout";
+import Title from "./components/title";
 import HomeBanner from "./components/homeBanner";
 
-export default function Home() {
+function Home({ state }) {
   return (
-    <Layout>
-      <HomeBanner />
-    </Layout>
+    <>
+      <Title />
+      <Head>
+        <meta name="description" content={state.frontity.description} />
+        <html lang="en" />
+      </Head>
+      <Global styles={globalStyles} />
+      <Layout>
+        <HomeBanner />
+      </Layout>
+    </>
   );
 }
+
+const globalStyles = css`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      "Droid Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+  a,
+  a:visited {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export default connect(Home);
